89be71c3e6bf5c0571fafb8c81c3d10d8b0266b0
thoadchapman <thoadchapman@gmail.com>
2026-04-29 22:55:53 -0300

    Refatora a classe Phrase para melhorar a representação e a manipulação de notas

--- musical_structures/musical_units/Phrase.py
+++ musical_structures/musical_units/Phrase.py
@@ -5,7 +5,27 @@ class Phrase:
         self.notes = notes
 
     def __repr__(self):
-        return f'{self.notes}'
+        return f'{", ".join(str(note) for note in self.notes)}'
+    
+    def __len__(self):
+        return len(self.notes)
+    
+    def __add__(self,other):
+        new_notes = self.notes.copy()
+        if isinstance(other,Note):
+            new_notes.append(other)
+        elif isinstance(other,Phrase):    
+            new_notes.extend(other.notes)
+        return Phrase(new_notes)
+    
+    def __iadd__(self,other):
+        new_notes = self.notes.copy()
+        if isinstance(other,Note):
+            new_notes.append(other)
+        elif isinstance(other,Phrase):    
+            new_notes.extend(other.notes)
+        self.notes = new_notes
+        return self
 
 if __name__ == "__main__":
     test1 = Note(60,4)
@@ -15,5 +35,10 @@ if __name__ == "__main__":
     notes = [test1,test2,test3]
 
     phrase1 = Phrase(notes)
+    phrase2 = Phrase(notes[::-1])
 
-    print(phrase1)
+    phrase3 = phrase1 + phrase2
+    print(f'add method test: {phrase3}')
+
+    phrase1 += test1
+    print(f'addi method test: {phrase1}')
