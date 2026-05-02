f7acd3f46115d603f0d4e6b923596c34c205e94a
thoadchapman <thoadchapman@gmail.com>
2026-05-02 17:57:11 -0300

    Refatora funções de geração de frases e candidatos, melhora a escolha da próxima nota e ajusta a lógica de pontuação melódica

--- musical_structures/melodics/melody.py
+++ musical_structures/melodics/melody.py
@@ -1,96 +1,55 @@
-import random
-from data import musical_data
-from infrastructure import infra
-
-from data import musical_data
+from random import randint, choice, choices
+from data.musical_data import PROBABILIDADES_INHARMONICAS as probabilidades, ESCALAS_MOLDE
+from musical_structures.musical_units.Phrase import Phrase
+from musical_structures.musical_units.Note import Note
+from musical_structures.melodics.ExpectationScore import ExpectationScore
 
 TONICA_BASE = 60
-ESCALA_BASE = musical_data.ESCALAS.get('pentatonica_maior')
+ESCALA_BASE = ESCALAS_MOLDE.get('major_pentatonic')
 
-def midi_para_grau(nota_midi, tonica_midi): # transforma a nota em midi para o seu equivalente em graus
-    if nota_midi is None: return 'rest'
-    return musical_data.INTERVALO_PARA_GRAU.get((nota_midi - tonica_midi)%12,'1')
+def gerar_frase(ritmo, tonica_midi=TONICA_BASE, escala=ESCALA_BASE): # recebe o ritmo e comeca a "eleicao" da proxima nota
+    PRIMEIRA_NOTA = Note(TONICA_BASE, ritmo[0])
+    notas = [PRIMEIRA_NOTA] 
+    for i in range(1,len(ritmo)): 
+        nota_atual = escolher_proxima_nota(notas[i-1].get_pitch(), tonica_midi, escala)
+        notas.append(Note(nota_atual,ritmo[i])) 
+    return Phrase(notas) 
 
-def grau_para_midi(grau, tonica_midi): # transforma o grau para o seu equivalente em midi
-    if grau == 'rest': return None
-    intervalo = musical_data.GRAU_PARA_INTERVALO.get((grau))
-    return tonica_midi + intervalo if intervalo is not None else None
-
-def gerar_frase_inteligente(ritmo, contexto, tonica_midi=TONICA_BASE, escala_graus=ESCALA_BASE): # recebe o ritmo e comeca a "eleicao" da proxima nota
-    frase_melodica = [] 
-    for duracao in ritmo: 
-        nota_atual = escolher_proxima_nota(contexto, tonica_midi, escala_graus) # escolhe a nota a ser tocada com base nos escores melodicos huron
-        frase_melodica.append(nota_atual) # adiciona na lista 
-        infra.atualizar_contexto(contexto, nota_atual)
-    
-    return frase_melodica # retorna a melodia (sem as duracoes)
-
-def gerar_candidatos(grau_anterior, tonica_midi, escala_graus): # procura no dicionario de probabilidades melodicas e cria uma lista com todas que podem ser escolhidas
-        transicoes_brutas = musical_data.PROBABILIDADES_MELODICAS.get(grau_anterior) 
-        transicoes_filtradas = {nota:chance for nota,chance in transicoes_brutas.items() if nota in escala_graus}
+def gerar_candidatos(ultimo_midi, tonica_midi=TONICA_BASE, escala=ESCALA_BASE): # procura no dicionario de probabilidades melodicas e cria uma lista com todas que podem ser escolhidas
+        print(f'----- NOVA NOTA -----')
         
+        ultimo_intervalo = ultimo_midi - tonica_midi
+        escala_intervalos = [int(i)-tonica_midi for i in escala]
+        possiveis_continuacoes = probabilidades.get(ultimo_intervalo)
+        print(f'POSSIVEIS CONTINUACOES ITEMS: {possiveis_continuacoes.items()}')
+        print(f'ESCALA: {escala_intervalos}')        
+        transicoes_filtradas = {intervalo:chance for intervalo, chance in possiveis_continuacoes.items() if intervalo in escala_intervalos}
+        
+        print(f'transicoes_filtradas: {transicoes_filtradas}')
         if not transicoes_filtradas: return [tonica_midi]
-
-        graus_possiveis = list(transicoes_filtradas.keys())
+        notas_possiveis = list(transicoes_filtradas.keys())
         pesos = list(transicoes_filtradas.values())
 
-        graus_escolhidos = random.choices(graus_possiveis, weights=pesos, k=5)
+        notas_escolhidas = choices(notas_possiveis, weights=pesos, k=1)
 
-        midi_escolhidos = [grau_para_midi(grau_escolhido, tonica_midi) for grau_escolhido in graus_escolhidos]
+        return notas_escolhidas
 
-        midi_escolhidos_limpos = [c for c in midi_escolhidos if c is not None]
+def score_melodico_avancado(nota_candidata, ultima_nota): # cria um placar que procura a proxima nota mais esperada
+    if not ultima_nota:
+        return 
+    medidor_de_expectativa = ExpectationScore(nota_candidata,ultima_nota)
+    score = medidor_de_expectativa.get_score()
+    print(f'score {ultima_nota} -> {nota_candidata}: {score}')
+    return score
 
-        return midi_escolhidos_limpos
-
-def score_melodico_avancado(pitch_candidato, contexto): # cria um placar que procura a proxima nota mais esperada
-    if not contexto['notas_midi']:
-        return 100
-
-    ultimo_pitch = contexto['notas_midi'][-1]
-    intervalo = pitch_candidato - ultimo_pitch
-
-    if intervalo == 0:
-        return 10 
-
-    score_proximidade = max(0, 100 - (abs(intervalo) * 8)) # determina se a proxima nota vai ser um salto 
-
-    score_reversao = 0
-    if contexto['intervalos'] and abs(contexto['intervalos'][-1]) > 4: 
-        if (intervalo * contexto['intervalos'][-1]) < 0: # determina se a proxima nota muda de direção
-            score_reversao = 50
-    return score_proximidade + score_reversao    
-
-def escolher_proxima_nota(contexto, tonica_midi, escala_graus): # decide a proxima nota a ser tocada com base na lista de candidatos 
-    ultima_nota = contexto['notas_midi'][-1] if contexto['notas_midi'] else tonica_midi
-    grau_anterior = midi_para_grau(ultima_nota,tonica_midi)
-    candidatos_midi = gerar_candidatos(grau_anterior,tonica_midi,escala_graus)
+def escolher_proxima_nota(ultimo_midi, tonica_midi, escala_graus): # decide a proxima nota a ser tocada com base na lista de candidatos 
+    candidatos_midi = gerar_candidatos(ultimo_midi,tonica_midi,escala_graus)
 
     if not candidatos_midi: return tonica_midi
-    nota_escolhida = sorted(candidatos_midi, key = lambda k:score_melodico_avancado(k,contexto), reverse=True)[random.randint(0,len(candidatos_midi)-1)] # pra ele não tender sempre as opcoes mais obvias
+    scores = sorted(candidatos_midi, key = lambda k:score_melodico_avancado(k,ultimo_midi)) # pra ele não tender sempre as opcoes mais obvias
+    indice_aleatorio = randint(0, len(scores)-1)
+    nota_escolhida = scores[indice_aleatorio]
+    return nota_escolhida+tonica_midi
 
-    return nota_escolhida
-
-def variar_melodia(frase_original, escala_midi): # mantem o ritmo e altera a tonalidade de uma nota
-    frase_alterada_melodica = [dict(evento) for evento in frase_original]
-    
-    indices_de_notas = [i for i, evento in enumerate(frase_alterada_melodica) if evento['type'] == 'note']
-
-    if not indices_de_notas or not escala_midi:
-        return frase_original
-
-    indice_a_trocar = random.choice(indices_de_notas)
-    
-    nota_antiga = frase_alterada_melodica[indice_a_trocar]['pitch']
-    nova_nota = nota_antiga
-
-    if len(escala_midi) > 1:
-      while nova_nota == nota_antiga:
-          nova_nota = random.choice(escala_midi)
-
-    frase_alterada_melodica[indice_a_trocar]['pitch'] = nova_nota
-    
-    print(f"A nota de índice {indice_a_trocar} foi escolhida. A nova nota eh {nova_nota}")
-
-
-    return frase_alterada_melodica
-
+def variar_melodia(frase_original, escala_midi): 
+    return frase_original.variar_melodia(escala_midi)
