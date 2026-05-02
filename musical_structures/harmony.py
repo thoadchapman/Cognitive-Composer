169cc678ad46803424c76d3b7dcc735627c077a7
thoadchapman <thoadchapman@gmail.com>
2026-05-02 17:58:19 -0300

    Refatora funções de harmonia e ritmo, renomeia métodos para melhorar a clareza e remove prints desnecessários

--- musical_structures/harmony.py
+++ musical_structures/harmony.py
@@ -1,9 +1,12 @@
 import random
+from data import musical_data
 
-def gerar_progressao_complexa(tonalidade, DICT_PROGRESSOES): # escolhe uma progressao comum e deixa mais complexa com substituicoes funcionais
-    if tonalidade == None: tonalidade = 'maior'
-    progressao_escolhida = random.choice(DICT_PROGRESSOES[tonalidade]['progressoes'])
-    substituicoes = DICT_PROGRESSOES[tonalidade]['substituicoes']
+PROGRESSOES = musical_data.DICT_PROGRESSOES
+
+def gerar_harmonia(tonalidade): # escolhe uma progressao comum e deixa mais complexa com substituicoes funcionais
+    if tonalidade == None: tonalidade = 'major'
+    progressao_escolhida = random.choice(PROGRESSOES[tonalidade]['progressoes'])
+    substituicoes = PROGRESSOES[tonalidade]['substituicoes']
     progressao_nova = []
     for acorde in progressao_escolhida:
         if acorde in substituicoes and random.random() < 0.4: # adiciona uma chance em 40% de substituir o acorde
--- musical_structures/rhythm.py
+++ musical_structures/rhythm.py
@@ -5,14 +5,12 @@ ONSET_WEIGHTS_BASE = musical_data.ONSET_COUNT_WEIGHTS
 RHYTHM_DNA_BASE = musical_data.RHYTHM_DNA_DATABASE
 POS_WEIGHTS_BASE = musical_data.POSITIONAL_PROPERTY_WEIGHTS
 
-def gerar_motivo_ritmico(onset_weights=ONSET_WEIGHTS_BASE, rhythm_dna=RHYTHM_DNA_BASE, pos_weights=POS_WEIGHTS_BASE): # decide uma sequencia de intervalos com base em ritmos e claves populares 
+def gerar_ritmo(onset_weights=ONSET_WEIGHTS_BASE, rhythm_dna=RHYTHM_DNA_BASE, pos_weights=POS_WEIGHTS_BASE): # decide uma sequencia de intervalos com base em ritmos e claves populares 
     num_notas = random.choices(list(onset_weights.keys()),weights = list(onset_weights.values()), k=1)[0]
 
     intervalos = random.choice(rhythm_dna[num_notas])
-    print(f'intervalos escolhidos: {intervalos}')
     
     primeiro_onset = random.choices(list(pos_weights['mainbeat onsets'].keys()),weights=list(pos_weights['mainbeat onsets'].values()), k=1)[0]
-    print(f'primeiro onset na beat: {primeiro_onset}')
     
     onsets = []
     onsets.append(int(primeiro_onset))
@@ -34,26 +32,4 @@ def onset_to_duracoes(onsets):
     return duracoes    
 
 def variar_ritmo(frase_original): # mantem as notas e altera a duracao de uma nota
-    frase_alterada = [dict(e) for e in frase_original]
-    indices_notas = [i for i, e in enumerate(frase_alterada) if e['type'] == 'note']
-    
-    if len(indices_notas) < 2:
-        return frase_original
-    
-    idx1, idx2 = random.sample(indices_notas, 2)
-    
-    duracoes = [0.25, 0.5, 1.0, 2.0]
-    duracao_antiga = frase_alterada[idx1]['duration']
-    nova_duracao = random.choice([d for d in duracoes if d != duracao_antiga])
-    
-    variacao_duracao = nova_duracao - duracao_antiga
-    
-    if frase_alterada[idx2]['duration'] - variacao_duracao <= 0:
-        print("Variação rítmica ignorada para evitar duração negativa.")
-        return frase_original
-        
-    frase_alterada[idx1]['duration'] = nova_duracao
-    frase_alterada[idx2]['duration'] -= variacao_duracao
-    
-    print(f"Índice {idx1} trocado. Variação de duração: {variacao_duracao:.2f}")
-    return frase_alterada
+    return frase_original.variar_ritmo()
